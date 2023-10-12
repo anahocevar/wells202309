1cf6bdba3cb3863bd2b58e3a3f91b11b8eb277c0
anahocevar <ahocevara@gmail.com>
2023-10-12 18:01:15 +0000

    Wrapped code into a function

--- database.py
+++ database.py
@@ -1,21 +1,28 @@
 from sqlalchemy import create_engine
 import os
 
+
 URL_DB = os.getenv("URL_DB")
 
-engine = create_engine(URL_DB)
+def query_db(depth_min, grad_min):
+    """Return wells based on criteria."""
+    
+    engine = create_engine(URL_DB)
 
-depth_min = 2000
-grad_min = 0.07
+    query = f"""
+        SELECT latitude, longitude, depth, gradient
+        FROM wells
+        WHERE depth > {depth_min} AND gradient > {grad_min};
+    """
 
-query = f"""
-    SELECT latitude, longitude, depth, gradient
-    FROM wells
-    WHERE depth > {depth_min} AND gradient > {grad_min};
-"""
+    conn = engine.connect()
+    results = conn.execute(query).fetchall()
+    conn.close()
 
-conn = engine.connect()
-results = conn.execute(query).fetchall()
-conn.close()
+    return results
 
-print(results)
+if __name__ == '__main__':
+    depth_min = 2000
+    grad_min = 0.075
+    print(query_db(depth_min, grad_min))
+    
