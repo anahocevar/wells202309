881c363730831713ff0019281f822ee9df777200
anahocevar <ahocevara@gmail.com>
2023-10-12 18:18:39 +0000

    Prevent injection attacks

--- database.py
+++ database.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, text
 import os
 
 
@@ -9,14 +9,14 @@ def query_db(depth_min, grad_min):
     
     engine = create_engine(URL_DB)
 
-    query = f"""
+    query = text("""
         SELECT latitude, longitude, depth, gradient
         FROM wells
-        WHERE depth > {depth_min} AND gradient > {grad_min};
-    """
+        WHERE depth > :depth_min AND gradient > :grad_min
+    """)
 
     with engine.connect() as conn:
-        results = conn.execute(query).fetchall()
+        results = conn.execute(query, depth_min=depth_min, grad_min=grad_min).fetchall()
         
     return results
 
